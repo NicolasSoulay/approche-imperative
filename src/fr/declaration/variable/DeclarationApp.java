c50b7cc31c468802da87f15823f648ee42fa5fd6
NicolasSoulay <soulaynicolas@gmail.com>
2023-05-15 13:11:32 +0200

    fin de tp

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,19 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main (String[] args) {
+        Byte monByte = 1;
+        Short monShort = 2;
+        Integer monInt = 3;
+        Long monLong = 444444444L;
+        Float monFloat = 5.1F;
+        Double monDouble = 6.2;
+        Character monChar = 8;
+        Boolean monBool = true;
+        String monString = "je suis un string";
+
+        String randomString= "Voici le résultat d’un calcul : 1+5=6";
+
+        System.out.println(randomString);
+    }
+}
